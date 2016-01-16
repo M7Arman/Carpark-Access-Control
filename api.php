3b40331b748d986f44634c5529fe7762975ca372
Arman Kostandyan <armankostandyan@gmail.com>
2016-01-16 20:45:45 +0400

    Impelemented Add Car and Delete Car APIs

--- api.php
+++ api.php
@@ -1,5 +1,5 @@
 <?php
-/* Require Slim and plugins */
+/* Require Slim and NotORM */
 require 'vendor/autoload.php';
 
 $app = new \Slim\App;
@@ -14,11 +14,13 @@ $dsn = $dbmethod.$dbname;
 $pdo = new PDO($dsn, $dbuser, $dbpass);
 $db = new NotORM($pdo);
 
-$app->post('/car', function ($request, $response, $args) use($db) {
-    //TODO: Impelement function
+$app->post('/car', function ($req, $res, $args) use($db) {
+    $car = $req->getParsedBody();
+    $result = $db->cars->insert($car);
+    return $res->write($result['id']);
 });
 
-$app->get('/cars', function () use($db) {
+$app->get('/cars', function ($req, $res, $args) use($db) {
     $cars = array();
     foreach($db->cars() as $car) {
         $cars[] = array(
@@ -28,12 +30,32 @@ $app->get('/cars', function () use($db) {
             'ticket' => $car['ticket']
         );
     }
-    //TODO: Add functionality to se header(in this case content-type)
-    echo json_encode($cars, JSON_FORCE_OBJECT);
+    return $res->withHeader(
+        'Content-Type',
+        'application/json'
+    )->write(json_encode($cars));
 });
 
-$app->delete('/car', function ($request, $response, $args) use($db) {
-    //TODO: Impelement function
+$app->delete('/car/{num}/{region}', function ($req, $res, $args) use($db) {
+    $num = $args['num'];
+    $region = $args['region'];
+    $car = $db->cars()->where(array("number" => $num, "region" => $region));
+    if($car->fetch()){
+        $result = $car->delete();
+        $responseBody = json_encode(array(
+            "status" => true,
+            "message" => "Car deleted successfully."
+        ));
+    } else {
+        $responseBody = json_encode(array(
+            "status" => false,
+            "message" => "Car with $num number and $region region does not exist."
+        ));
+    }
+    return $res->withHeader(
+        'Content-Type',
+        'application/json'
+    )->write($responseBody);
 });
 
 /* Run the application */
